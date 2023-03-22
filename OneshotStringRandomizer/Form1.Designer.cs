238857b41750b9cb1987ce55d527996dc5a0bad8
Sum <literallysum@gmail.com>
2023-03-22 19:10:22 +0500

    Add project files.

---
+++ OneshotStringRandomizer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace OneshotStringRandomizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(466, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\OneShot";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OneShot installation path";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 51);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Preserve speech tags";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(12, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(170, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Randomize images with text";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(484, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.ShortcutsEnabled = false;
+            this.richTextBox1.Size = new System.Drawing.Size(554, 122);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "Ready";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(554, 44);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "BEGIN THE TOMFOOLERY";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 281);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "OneShot String Randomizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OneshotStringRandomizer/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace OneshotStringRandomizer
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void sendLog(string text, bool error = false)
+        {
+            this.richTextBox1.Select(this.richTextBox1.TextLength, 0);
+            if (error) this.richTextBox1.SelectionColor = Color.Red;
+            this.richTextBox1.AppendText("\r\n" + text);
+            this.richTextBox1.SelectionColor = Color.Black;
+        }
+
+        CommonOpenFileDialog directoryDialog = new CommonOpenFileDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            directoryDialog.InitialDirectory = this.textBox1.Text;
+            directoryDialog.IsFolderPicker = true;
+            if (directoryDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                this.textBox1.Text = directoryDialog.FileName;
+            }
+        }
+
+
+        Random rng = new Random();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Control[] controlElements = { this.textBox1, this.button1, this.checkBox1, this.checkBox2, this.button2 };
+            foreach (Control obj in controlElements)
+            {
+                obj.Enabled = false;
+            }
+            string gameDirectory = this.textBox1.Text;
+            bool preserveSpeechTags = this.checkBox1.Checked;
+            bool randomizeImages = this.checkBox2.Checked;
+            try
+            {
+                sendLog("Reading "+ gameDirectory + "\\Languages\\fr.po");
+                string[] file = File.ReadAllLines(gameDirectory + "\\Languages\\fr.po");
+                sendLog("Translating strings...");
+                int li = -1;
+                List<string> strings = new List<string>();
+                foreach (string line in file)
+                {
+                    li++;
+                    if (line.StartsWith("msgstr"))
+                    {
+                        file[li] = "msgstr " + file[li-1].Substring(6);
+                        strings.Add(file[li - 1].Substring(6));
+                    }
+                }
+                sendLog("Sorting strings...");
+                List<string> sort_tags = new List<string>();
+                List<string> sort_strings = new List<string>();
+                foreach (string str in strings)
+                {
+                    //strip away quotation marks
+                    string qs = str.Substring(1, str.Length - 2);
+                    //idk why this is here but it might fix something. probably
+                    qs = qs.Trim();
+                    if (qs.StartsWith("@") && preserveSpeechTags)
+                    {
+                        //string has a speech tag, we need to split it
+                        string speechTag = qs.Split(' ')[0];
+                        sort_tags.Add(speechTag);
+                        //this condition exists because theres this one (or maybe even more idfk) string that has a speech tag and nothing else
+                        if (qs.Length == speechTag.Length) sort_strings.Add(qs.Substring(speechTag.Length));
+                        else sort_strings.Add(qs.Substring(speechTag.Length + 1));
+                    }
+                    else
+                    {
+                        sort_tags.Add(null);
+                        sort_strings.Add(qs);
+                    }
+                }
+                sendLog("Shuffling strings...");
+                sort_strings.Shuffle();
+                sendLog("Updating translation file...");
+                li = -1;
+                int ti = -1;
+                foreach (string line in file)
+                {
+                    li++;
+                    if (line.StartsWith("msgstr"))
+                    {
+                        ti++;
+                        if (sort_tags[ti] != null && preserveSpeechTags)
+                        {
+                            file[li] = "msgstr \"" + sort_tags[ti]+" " + sort_strings[ti]+"\"";
+                        } else
+                        {
+                            file[li] = "msgstr \"" + sort_strings[ti] + "\"";
+                        }
+                    }
+                }
+                //hardcoded rewrites for language code
+                file[5] = "msgid \"rand\"";
+                file[6] = "msgstr \"rand\"";
+                sendLog("Writing to " + gameDirectory + "\\Languages\\rand.po");
+                File.WriteAllLines(gameDirectory + "\\Languages\\rand.po", file);
+                sendLog("Updating language_fonts.ini");
+                string[] langfonts = File.ReadAllLines(gameDirectory + "\\Languages\\language_fonts.ini");
+                string font = "rand=Terminus (TTF)";
+                if (Array.Find(langfonts, fontdef => fontdef == font) != font)
+                {
+                    File.AppendAllText(gameDirectory + "\\Languages\\language_fonts.ini", font);
+                }
+                //string randomization over, begin image randomization
+                if (!randomizeImages) return;
+                //theres probably an easier way to do these three but i dont care
+                string[] introImages = {"felix", "instruction1", "instruction2", "instruction3", "instruction4"};
+                string[] bookImages = { "clover", "dice", "fauna", "phosphor1", "phosphor2", "phosphor3", "phosphor4", "prophet", "prophetbot", "sketch"};
+                string[] journalImages = { "c1", "c2", "c3", "c4", "c5", "c6", "c7", "final", "s1", "s2", "s3", "s4", "save", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10", "t11", "t12", "t13", "t14", "t15", "t16" };
+                //randomize intro images
+                var shuffleIntro = new string[introImages.Length]; Array.Copy(introImages, shuffleIntro, introImages.Length); rng.Shuffle(shuffleIntro);
+                int i = -1;
+                if (!Directory.Exists(gameDirectory + "\\Graphics\\Pictures\\rand\\")) Directory.CreateDirectory(gameDirectory + "\\Graphics\\Pictures\\rand\\");
+                foreach (string image in shuffleIntro)
+                {
+                    i++;
+                    sendLog("Writing " + introImages[i] + " as " + shuffleIntro[i]);
+                    File.Copy(gameDirectory + "\\Graphics\\Pictures\\" + introImages[i] + ".png", gameDirectory + "\\Graphics\\Pictures\\rand\\" + shuffleIntro[i] + ".png", true);
+                }
+                //randomize book images
+                var shuffleBook = new string[bookImages.Length]; Array.Copy(bookImages, shuffleBook, bookImages.Length); rng.Shuffle(shuffleBook);
+                i = -1;
+                foreach (string image in shuffleBook)
+                {
+                    i++;
+                    sendLog("Writing " + bookImages[i] + " as " + shuffleBook[i]);
+                    File.Copy(gameDirectory + "\\Graphics\\Pictures\\book_" + bookImages[i] + ".png", gameDirectory + "\\Graphics\\Pictures\\rand\\book_" + shuffleBook[i] + ".png", true);
+                }
+                //randomize journal images
+                var shuffleJournal = new string[journalImages.Length]; Array.Copy(journalImages, shuffleJournal, journalImages.Length); rng.Shuffle(shuffleJournal);
+                i = -1;
+                if (!Directory.Exists(gameDirectory + "\\Graphics\\Journal\\rand\\")) Directory.CreateDirectory(gameDirectory + "\\Graphics\\Journal\\rand\\");
+                foreach (string image in shuffleJournal)
+                {
+                    i++;
+                    sendLog("Writing " + journalImages[i] + " as " + shuffleJournal[i]);
+                    File.Copy(gameDirectory + "\\Graphics\\Journal\\" + journalImages[i] + ".bmp", gameDirectory + "\\Graphics\\Journal\\rand\\" + shuffleJournal[i] + ".bmp", true);
+                }
+            } catch (Exception err)
+            {
+                sendLog(err.GetType().ToString()+": "+err.Message,true);
+                sendLog(err.StackTrace,true);
+            }
+            sendLog("Randomization complete!");
+            foreach (Control obj in controlElements)
+            {
+                obj.Enabled = true;
+            }
+        }
+    }
+
+    static class RandomExtensions
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
